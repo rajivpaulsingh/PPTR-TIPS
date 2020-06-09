9ebe0f072dc57bf476294669cd352bbbc56878c0
Singh <singh2rajiv@dgb-c02vh08bh.bcg.com>
2020-06-09 13:59:37 -0700

    Code to emulate devices

--- example.js
+++ example.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const devices = require("puppeteer/DeviceDescriptors");
 
 describe("Tips and Tricks Test Suite", () => {
 
@@ -15,13 +16,22 @@ describe("Tips and Tricks Test Suite", () => {
         await browser.close();
     })
     
+    //Take full page screenshot
     it("Screenshot test", async function() {
         await page.goto("http://www.example.com", { waitUntil: "networkidle0" });
         await page.screenshot({ path: "example.png", fullPage: true });
     });
 
+    //Convert webpage to pdf
     it("PDF test", async function() {
         await page.goto("http://www.example.com", { waitUntil: "networkidle0" });
         await page.pdf({ path: "example.pdf", format: "A4"});
+    });
+
+    //Emulate devices
+    it("Emulate devices test", async function() {
+        await page.emulate(puppeteer.devices["iPhone X"]);
+        await page.goto("https://pptr.dev");
+        await page.waitFor(3000);
     });
 })
